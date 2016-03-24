d608b34764851737683d9f5402ae1624fce69885
Permanent Bad Ass <dads2busy@gmail.com>
2016-03-24 11:13:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 
 if [ ! -f ~/.bash_profile ]; then
     echo "~/.bash_profile not found"
-    cp ~/git/sdal_configs/.bash_profile ~/
+    cp ~/sdal_configs/.bash_profile ~/
     echo "~/.bash_profile created"
 fi
 
 if [ ! -f ~/.bashrc ]; then
     echo "~/.bashrc not found"
-    cp ~/git/sdal_configs/.bashrc ~/
+    cp ~/sdal_configs/.bashrc ~/
     echo "~/.bashrc created"
 fi
 
 
-echo "Added by sdal_configs setup" >> ~/.bashrc
+echo "\"Added by sdal_configs setup\"" >> ~/.bashrc
 
-echo "source ~/git/sdal_configs/.bash_aliases_sdal" >> ~/.bashrc
+echo "source ~/sdal_configs/.bash_aliases_sdal" >> ~/.bashrc
