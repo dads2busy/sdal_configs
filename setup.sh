d66f5805db35455d7d7c6aa75cbb092a0bc67f46
Permanent Bad Ass <dads2busy@gmail.com>
2016-03-24 11:31:05 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+# .BASH_PROFILE & .BASHRC
+
 if [ ! -f ~/.bash_profile ]; then
     echo "~/.bash_profile not found"
     cp ~/sdal_configs/.bash_profile ~/
@@ -12,9 +14,25 @@ if [ ! -f ~/.bashrc ]; then
     echo "~/.bashrc created"
 fi
 
-
 echo "\"Added by sdal_configs setup\"" >> ~/.bashrc
 
 echo "source ~/sdal_configs/.bash_aliases_sdal" >> ~/.bashrc
 
-source ~/sdal_configs/make_ssh_config.sh
+# SSH CONFIG FILE
+
+if [ ! -f ~/.ssh/config_home ]; then
+    echo "~/.ssh/config_home not found"
+    touch ~/.ssh/config_home
+    echo "~/.ssh/config_home created"
+fi
+
+cat ~/.ssh/config_home > ~/.ssh/config
+printf "\n\n# SDAL CONFIG BELOW\n\n">> ~/.ssh/config
+
+# use user supplied pid
+sed -i -e "s/<your_pid>/$1/g" ~/sdal_configs/config
+
+cat ~/sdal_configs/config >> ~/.ssh/config
+
+# reset the file
+sed -i -e "s/$1/<your_pid>/g" ~/sdal_configs/config
